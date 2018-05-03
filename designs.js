8c5ee6f1a9c2c14f29b6536c4c4c96f56c46fc67
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2018-05-04 00:43:33 +0200

    Implement makeGrid function

--- designs.js
+++ designs.js
@@ -1,3 +1,23 @@
-function makeGrid() {
-    
-}
+/*Use IIFE to avoid polluting global scope*/
+(function () {
+    /*Get elements*/
+    const setSizeButton = document.getElementById('set-size-button');
+    const widthInputField = document.getElementById('width-input-field');
+    const heightInputField = document.getElementById('height-input-field');
+    const table = document.getElementById('canvas');
+
+    /*Declare functions*/
+    function makeGrid (aWidth, aHeight) {
+        for (let i = 0; i < aHeight; i++) {
+            const row = table.appendChild(document.createElement('tr'));
+            for (let j = 0; j < aWidth; j++) {
+                row.appendChild(document.createElement('td'));
+            }
+        }
+    }
+
+    /*Add event listeners*/
+    setSizeButton.addEventListener('click', function () {
+        makeGrid(widthInputField.value, heightInputField.value);
+    });
+})();
