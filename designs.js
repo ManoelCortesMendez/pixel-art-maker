9aa089bff0014ae4c050ac67ff4e5257207b169b
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2018-05-04 15:55:43 +0200

    Implement clear canvas function

--- designs.js
+++ designs.js
@@ -8,8 +8,10 @@
     const colorPicker = document.getElementById('color-input');
     const colorPickerContainer = document.getElementById('container-color-input');
     let colorPicked = "#000000";
+    const clearButton = document.getElementById('clear');
 
     /*Declare functions*/
+    // TODO: if I don't use input object, I shouldn't use table obj in fun
     function makeGrid (aWidth, aHeight) {
         for (let i = 0; i < aHeight; i++) {
             const row = table.appendChild(document.createElement('tr'));
@@ -21,6 +23,7 @@
 
     /*Add event listeners*/
     setSizeButton.addEventListener('click', function () {
+        table.innerHTML = ''; //Reset table to empty
         makeGrid(widthInputField.value, heightInputField.value);
     });
     /*Trigger listener on input color change*/
@@ -28,5 +31,18 @@
         colorPicked = this.value;
         colorPickerContainer.style.background = colorPicked;
     });
-    
+    // TODO: Change listeners to call dedicated functions? Or leave as is?
+    table.addEventListener('click', function (event) {
+        if (event.target.nodeName === 'TD') {
+            const cellClicked = event.target;
+            cellClicked.style.background = colorPicked;
+        }
+    });
+    clearButton.addEventListener('click', function() {
+        for (const row of table.children) {
+            for (const col of row.children) {
+                col.style.background = "#FFFFFF";
+            }
+        }
+    });
 })();
